2012f940f99f848c557f32107e23451009f9560c
Keshav S <s.keshav13142@gmail.com>
2023-03-20 14:26:42 +0530

    Updated stock problem

--- src/best_stock.ts
+++ src/best_stock.ts
@@ -6,7 +6,7 @@ function stockProfitBrute(stocks: number[]): number {
   //   if (maxPrice > minPrice) return maxPrice - minPrice;
   //   return 0;
 
-  let maxProfit = -Infinity;
+  let maxProfit = 0;
   for (let i = 0; i < stocks.length - 1; i++) {
     for (let j = i + 1; j < stocks.length; j++) {
       const profit = stocks[j] - stocks[i];
@@ -14,7 +14,23 @@ function stockProfitBrute(stocks: number[]): number {
     }
   }
 
-  return maxProfit === -Infinity ? 0 : maxProfit;
+  return maxProfit;
 }
 
-console.log(stockProfitBrute([7, 1, 5, 3, 6, 4]));
+function greedyStockProfit(stocks: number[]): number {
+  if (stocks.length === 1) return 0;
+  let maxProfit = 0;
+  let minPrice = stocks[0];
+  for (let i = 0; i < stocks.length; i++) {
+    minPrice = Math.min(stocks[i], minPrice);
+    const currProfit = stocks[i + 1] - minPrice;
+    if (currProfit > 0) {
+      maxProfit = Math.max(currProfit, maxProfit);
+    }
+  }
+
+  return maxProfit;
+}
+
+console.log(greedyStockProfit([7, 1, 5, 3, 6, 4]));
+// 7, 1, 5, 3, 6, 4;
