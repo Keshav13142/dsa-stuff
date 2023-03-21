1e96e67d57c109e768f8b6dc39596966d4384852
Keshav S <s.keshav13142@gmail.com>
2023-03-21 21:01:00 +0530

    Added thePrimeagen's dsa-repo

---
+++ primeagen/src/day1/MazeSolver.ts
@@ -0,0 +1,12 @@
+function walk(): boolean {
+    return false;
+}
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    return [{ x: 0, y: 0 }];
+}
